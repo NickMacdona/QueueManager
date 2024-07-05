591d54e9cff8e7e7633bd176b50daf2be80af868
NickMacdona <117294653+NickMacdona@users.noreply.github.com>
2024-07-06 00:17:36 +0100

    Creation of queue class and IQueue interface

---
+++ QueueManager/IQueue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueManager
+{
+    public interface IQueue
+    {
+        void AddJob(Job job);
+        Job? Dequeue();
+        string QueueName { get; }
+        int Type { get; }
+    }
+}
---
+++ QueueManager/Queue.cs
@@ -0,0 +1,56 @@
+﻿namespace QueueManager
+{
+    //consider use of a miniheap in the future to scale better into larger queues
+    //todo implement an index dictionary
+    //todo implement logging
+    public class Queue : IQueue
+    {
+        private readonly string _queueName;
+        private readonly int _queueType;
+        private readonly SortedDictionary<int, Job> _queue = new SortedDictionary<int, Job>();
+
+        public Queue(string name, int type)
+        {
+            //todo I dont like this implementation and we should have all jobs be in one queue. Lets index on both priority and on type instead
+
+            _queueName = name;
+            _queueType = type;
+        }
+
+        public void AddJob(Job job)
+        {
+            _queue.Add(GetNewId(), job);
+        }
+
+        public Job? Dequeue()
+        {
+            if (_queue.Count > 0)
+            {
+                KeyValuePair<int, Job> firstJob = _queue.First();
+                _queue.Remove(firstJob.Key);
+                return firstJob.Value;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private int GetNewId()
+        {
+            if (_queue.Count == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                int newId = _queue.Last().Key + 1;
+                return newId;
+            }
+        }
+
+        public string QueueName => _queueName;
+
+        public int Type => _queueType;
+    }
+}
